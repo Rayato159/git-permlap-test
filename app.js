4b837f102a431f32033e970bb36a1c9c5bd55355
Permlap <permlapteam@gmail.com>
2023-07-26 22:00:01 +0700

    add protect branch

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-console.log("stupid calculator-123");
-=======
 import { add } from "./utils/add";
 
 console.log("stupid calculator");
 console.log("add protect branch");
 const result1 = add(1, 2);
 console.log(result1);
->>>>>>> develop
